8e50586431dd88150ce5823a9390c6e45c27eb3a
wtetsu <tewatana@gmail.com>
2019-01-14 22:55:45 +0900

    Update packages

--- vue-example/webpack.config.js
+++ vue-example/webpack.config.js
@@ -6,29 +6,25 @@ module.exports = {
     path: __dirname,
     filename: "./dist/app.js"
   },
-  plugins: [
-  ],
   resolve: {
     alias: {
       vue: "vue/dist/vue.js"
     }
   },
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
         loader: "babel-loader"
-      }
-    ],
-    rules: [
+      },
       {
         test: /\.js$/,
         exclude: /(node_modules|bower_components)/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: ['env']
+            presets: ["env"]
           }
         }
       }
